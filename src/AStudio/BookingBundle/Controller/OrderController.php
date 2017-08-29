2c410fe9fd75a9d3ffa83d731841b971f0100c64
arthurgeay <arthurgeay.contact@gmail.com>
2017-08-29 19:53:52 +0200

    Implémentation du modèle triple héritage de layout twig

--- src/AStudio/BookingBundle/Controller/OrderController.php
+++ src/AStudio/BookingBundle/Controller/OrderController.php
@@ -3,14 +3,11 @@
 namespace AStudio\BookingBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpFoundation\Response;
 
 class OrderController extends Controller
 {
     public function indexAction()
     {
-        $content = $this->get('templating')->render('AStudioBookingBundle:Order:index.html.twig');
-        
-        return new Response($content);
+        return $this->render('AStudioBookingBundle:Order:index.html.twig');
     }
 }
